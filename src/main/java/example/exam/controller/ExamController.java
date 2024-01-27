e6c71e4aa9ae26522f5bc3628352036b8548e153
kkasu <kkasu1992@gmail.com>
2024-01-28 03:39:33 +0900

    api 테스트

--- src/main/java/example/exam/controller/ExamController.java
+++ src/main/java/example/exam/controller/ExamController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ExamController {
@@ -22,5 +23,33 @@ public class ExamController {
         model.addAttribute("data", data);
 
         return "mvc-hello";
+    }
+
+    @GetMapping("string-hello")
+    @ResponseBody   // HttpMessageConverter -> StringConverter
+    public String Hello(@RequestParam("data") String data, Model model) {
+
+        return "hello " + data;
+    }
+
+    @GetMapping("api-hello")
+    @ResponseBody   // HttpMessageConverter -> JsonConverter
+    public Hello apiHello(@RequestParam("data") String data) {
+        Hello hello = new Hello();
+        hello.setName(data);
+
+        return hello;
+    }
+
+    static class Hello {
+        String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
