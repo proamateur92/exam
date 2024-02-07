5dd1a6fadee9021a56ecf0c3fcb875d76c6ce10a
kkasu <kkasu1992@gmail.com>
2024-02-07 23:05:44 +0900

    레포지토리 변경

---
+++ src/main/java/example/exam/SpringConfig.java
@@ -0,0 +1,32 @@
+package example.exam;
+
+import example.exam.repository.JdbcMemberRepository;
+import example.exam.repository.MemberRepository;
+import example.exam.repository.MemoryMemberRepository;
+import example.exam.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
+    }
+}
